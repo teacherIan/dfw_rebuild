34e33e148f402634e2626eaea710991131564cb9
Ian Malloy <iansmalloy@gmail.com>
2025-11-29 21:07:28 -0500

    updates...

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Root, Container, Text } from '@react-three/uikit'
 import { Button } from '@react-three/uikit-default'
 import Logo from './Logo.jsx'
 import Z from './Z.jsx'
+import Logo_new from './Logo_new.jsx'
 
 function App() {
   const [animating, setAnimating] = useState(true)
@@ -14,21 +15,22 @@ function App() {
       <Root sizeX={1} sizeY={1} pixelSize={0.01} anchorX="center" anchorY="center">
         <Container flexDirection="row" alignItems="center" width="100%" height="100%" justifyContent="center" gap={50}>
           <Container flexDirection="column" alignItems="center" justifyContent="center" gap={20}>
-            <Button variant='outline' positionZ={-50} onClick={() => setAnimating(!animating)}>
-              <Text>Gallery</Text>
-            </Button>
-            <Button positionZ={-50} onClick={() => setAnimating(!animating)}>
-              <Text>Contact</Text>
-            </Button>
+            {/*<Button variant='outline' positionZ={-50} onClick={() => setAnimating(!animating)}>*/}
+            {/*  <Text>Gallery</Text>*/}
+            {/*</Button>*/}
+            {/*<Button positionZ={-50} onClick={() => setAnimating(!animating)}>*/}
+            {/*  <Text>Contact</Text>*/}
+            {/*</Button>*/}
           </Container>
-          <Logo url="/splats/logo.spz" animating={animating} />
+          {/*<Logo url="/splats/fixed_model_compressed.spz" animating={animating} />*/}
+            <Logo_new url="/splats/dfw_rotation_fixed.spz" animating={animating} />
           <Container flexDirection="column" alignItems="center" justifyContent="center" gap={20}>
-            <Button variant='outline' positionZ={-50} onClick={() => setAnimating(!animating)}>
-              <Text positionZ={-50} >Gallery Two</Text>
-            </Button>
-            <Button positionZ={-50} onClick={() => setAnimating(!animating)}>
-              <Text>Values</Text>
-            </Button>
+            {/*<Button variant='outline' positionZ={-50} onClick={() => setAnimating(!animating)}>*/}
+            {/*  <Text positionZ={-50} >Gallery Two</Text>*/}
+            {/*</Button>*/}
+            {/*<Button positionZ={-50} onClick={() => setAnimating(!animating)}>*/}
+            {/*  <Text>Values</Text>*/}
+            {/*</Button>*/}
           </Container>
         </Container>
       </Root>
